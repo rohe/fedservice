e49cf4618a469253022c4b20c7e4ffab420b9ee9
roland <roland@catalogix.se>
2019-12-10 01:01:03 +0100

    Modified get_chains script

--- script/get_chains.py
+++ script/get_chains.py
@@ -19,20 +19,27 @@ if __name__ == '__main__':
 
     trusted_roots = json.loads(open(args.trusted_roots_file).read())
 
+    # Creates an entiy that can do the collecting of information
     federation_entity = FederationEntity(
         'issuer', trusted_roots=trusted_roots,
         entity_type=args.entity_type,
         opponent_entity_type=args.opponent_entity_type)
 
+    if args.insecure:
+        federation_entity.collector.insecure = args.insecure
+
     jws = federation_entity.get_configuration_information(args.url)
     metadata = verify_self_signed_signature(jws)
 
-    _tree = federation_entity.collect_statement_chains(metadata['iss'], jws)
+    _tree = federation_entity.collect_statement_chains(metadata['iss'], metadata)
     chains = branch2lists(_tree)
+    for c in chains:
+        c.append(jws)
     statements = [eval_chain(c, federation_entity.key_jar, args.opponent_entity_type) for c in
                   chains]
 
     for statement in statements:
         print(20 * "=", statement.fo, 20 * "=")
-        # pretty print JSON
-        print(json.dumps(statement.metadata, sort_keys=True, indent=4))
+        for ver in statement.verified_chain:
+            # pretty print JSON
+            print(json.dumps(ver, sort_keys=True, indent=4))
