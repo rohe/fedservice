dfd1b09d91d7e7f89f4e6c1d317fe0eb1c1133e9
roland <roland@catalogix.se>
2020-02-24 10:40:49 +0100

    Added missing files.

--- tests/base_data/make.py
+++ tests/base_data/make.py
@@ -7,16 +7,16 @@ from fedservice.metadata_api.fs2 import FSEntityStatementAPI
 BASE_PATH = os.path.abspath(os.path.dirname(__file__))
 
 
-# def make_entity_id(item):
-#     return "https://127.0.0.1:5000/eid/{}".format(item)
+def make_entity_id(item):
+    return "https://{}".format(item)
 
 
 for iss in os.listdir(BASE_PATH):
     path = os.path.join(BASE_PATH, iss)
     if os.path.isdir(path):
-        fse = FSEntityStatementAPI(iss, BASE_PATH)
+        fse = FSEntityStatementAPI(BASE_PATH, iss=iss)
         # fse.make_entity_id = make_entity_id
-        fse.load_jwks(iss, iss)
+        fse.load_jwks(iss, iss, make_entity_id(iss))
         for sub in os.listdir(path):
             sub_path = os.path.join(path, sub)
             if os.path.isdir(sub_path):
