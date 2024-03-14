a7829ab75cc2b133aaa6e1ad431b190c1f601160
Roland Hedberg <roland@catalogix.se>
2024-03-14 21:31:42 +0100

    Added client_authn_methods.

--- src/fedservice/appclient/__init__.py
+++ src/fedservice/appclient/__init__.py
@@ -1,20 +1,21 @@
+from json import JSONDecodeError
 import logging
 import re
-from json import JSONDecodeError
 from typing import Callable
 from typing import Optional
 from typing import Union
 
 from cryptojwt import KeyJar
 from cryptojwt.key_jar import init_key_jar
+from idpyoidc.client.client_auth import client_auth_setup
+from idpyoidc.client.client_auth import method_to_item
 from idpyoidc.client.defaults import DEFAULT_OIDC_SERVICES
 from idpyoidc.client.defaults import SUCCESSFUL
 from idpyoidc.client.exception import OidcServiceError
-from idpyoidc.client.oidc.registration import Registration
 from idpyoidc.client.rp_handler import RPHandler
-from idpyoidc.client.service import init_services
 from idpyoidc.client.service import REQUEST_INFO
 from idpyoidc.client.service import Service
+from idpyoidc.client.service import init_services
 from idpyoidc.client.service_context import ServiceContext
 from idpyoidc.client.util import do_add_ons
 from idpyoidc.client.util import get_deserialization_method
@@ -79,8 +80,17 @@ class ClientEntity(ClientUnit):
                 entity_id=self.entity_id
             )
 
+        self.setup_client_authn_methods(config)
+
         if "add_ons" in config:
             do_add_ons(config["add_ons"], self._service)
+
+    def setup_client_authn_methods(self, config):
+        if config and "client_authn_methods" in config:
+            _methods = config.get("client_authn_methods")
+            self.context.client_authn_methods = client_auth_setup(method_to_item(_methods))
+        else:
+            self.context.client_authn_methods = {}
 
     def get_services(self, *arg):
         return self._service
@@ -333,7 +343,8 @@ class ClientEntity(ClientUnit):
             return err_resp
         else:
             logger.error(f"Error response ({reqresp.status_code}): {reqresp.text}")
-            raise OidcServiceError(f"HTTP ERROR: {reqresp.text} [{reqresp.status_code}] on {reqresp.url}")
+            raise OidcServiceError(
+                f"HTTP ERROR: {reqresp.text} [{reqresp.status_code}] on {reqresp.url}")
 
 
 def init_oidc_rp_handler(app, dir: Optional[str] = ""):
