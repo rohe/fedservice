48e15f830ede003471f7b816d59fc47073cf4b49
roland <roland@catalogix.se>
2024-03-22 09:31:36 +0100

    Fixed configuration error.

--- example/test_config.py
+++ example/test_config.py
@@ -1,16 +1,14 @@
 #!/usr/bin/env python3
 import sys
 
-from fedservice.combo import FederationCombo
-
-from fedservice.utils import make_federation_combo
 from idpyoidc.util import load_config_file
 
-from utils import load_values_from_file
+from fedservice.combo import FederationCombo
+from fedservice.utils import load_values_from_file
+from fedservice.utils import make_federation_combo
 
 subdir = sys.argv[1]
-config_file =  sys.argv[2]
-
+config_file = sys.argv[2]
 
 _cnf = load_values_from_file(load_config_file(f"{subdir}/{config_file}"))
 entity = make_federation_combo(**_cnf["entity"])
@@ -19,4 +17,3 @@ if isinstance(entity, FederationCombo):
     print(entity["federation_entity"].keyjar.export_jwks())
 else:
     print(entity.keyjar.export_jwks())
-
