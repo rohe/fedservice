8bb6444aec4a838072a5428502802ee76c06247f
Mikael Frykholm <mifr@sunet.se>
2024-08-26 10:48:41 +0200

    Should be parameter, not hard coded.

--- docker/start.sh
+++ docker/start.sh
@@ -2,7 +2,7 @@
 
 for file in conf.json views.py; do
 	if [ ! -f /"${1}"/"${file}" ]; then
-		echo "No ${file} found, copying to /wallet_provider/"
+		echo "No ${file} found, copying to /${1}/"
 		cp /fedservice/setup_federation/"${1}"/"${file}" /"${1}"/
 	else
 		echo "${file} found, leaving alone. Beware when upgrading."
