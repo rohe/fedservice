57e7c06f9493ab03b95eeb51699f8fc2fca08c13
roland <roland@catalogix.se>
2024-09-27 09:08:41 +0200

    A step on the way to renaming setup_federation to dc4eu_federation

---
+++ dc4eu_federation/issuer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import json
+import sys
+
+from idpyoidc.util import load_config_file
+
+if __name__ == "__main__":
+    dir_name = sys.argv[1]
+
+    cnf = load_config_file(f"{dir_name}/conf.json")
+    _ids = list(cnf["entity"]["trust_mark_entity"]["kwargs"]["trust_mark_specification"].keys())
+
+    print(json.dumps({cnf["entity"]["entity_id"]: _ids}))
