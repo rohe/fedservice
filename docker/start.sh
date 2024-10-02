fb3140284b877a59f199550fb8c523be7ada72ee
Mikael Frykholm <mifr@sunet.se>
2024-10-02 16:05:48 +0200

    Change path to example files.

--- docker/start.sh
+++ docker/start.sh
@@ -3,11 +3,11 @@
 for file in conf.json views.py; do
 	if [ ! -f /"${1}"/"${file}" ]; then
 		echo "No ${file} found, copying to /${1}/"
-		cp /fedservice/setup_federation/"${1}"/"${file}" /"${1}"/
+		cp /fedservice/dc4eu_federation/"${1}"/"${file}" /"${1}"/
 	else
 		echo "${file} found, leaving alone. Beware when upgrading."
 
 	fi
 done
-echo "Starting wallet_provider."
-/fedservice/setup_federation/entity.py "$@"
+echo "Starting $@."
+/fedservice/dc4eu_federation/entity.py "$@"
